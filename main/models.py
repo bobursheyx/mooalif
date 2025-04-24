5b998b1bc1e45e03ec14682ab18a9c0f7c1f2116
bobursheyx <olponovboburjon2@gmail.com>
2025-04-24 02:53:10 -0700

    number

--- main/models.py
+++ main/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Ariza(models.Model):
     ismi = models.CharField(max_length=100)
     email = models.EmailField()
+    telefon_nomer = models.IntegerField
     xabar = models.TextField()
 
     def __str__(self):

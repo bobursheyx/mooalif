8587bf156ca1151071b9c4550856e299fc388110
bobursheyx <olponovboburjon2@gmail.com>
2025-04-27 02:05:05 -0700

    tugadi

--- main/admin.py
+++ main/admin.py
@@ -2,4 +2,7 @@
 from django.contrib import admin
 from .models import Ariza
 
-admin.site.register(Ariza)
+@admin.register(Ariza)
+class ArizaAdmin(admin.ModelAdmin):
+    list_display = ('ismi', 'email', 'telefon_nomer', 'xabar')
+    fields = ('ismi', 'email', 'telefon_nomer', 'xabar')
--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Ariza(models.Model):
     ismi = models.CharField(max_length=100)
     email = models.EmailField()
-    telefon_nomer = models.IntegerField
+    telefon_nomer = models.IntegerField(max_length=20, blank=True, null=True)
     xabar = models.TextField()
 
     def __str__(self):
